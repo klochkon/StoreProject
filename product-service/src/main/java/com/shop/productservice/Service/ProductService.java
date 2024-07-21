85a16f1e291a45d26551e7e1610cb424e2476ecd
ya <nikita6969228@gmail.com>
2024-07-21 21:31:13 +0300

    21.07 cache

--- product-service/src/main/java/com/shop/productservice/Service/ProductService.java
+++ product-service/src/main/java/com/shop/productservice/Service/ProductService.java
@@ -3,6 +3,9 @@ package com.shop.productservice.Service;
 import com.shop.productservice.Model.Product;
 import com.shop.productservice.Repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -13,16 +16,33 @@ public class ProductService {
 
     private final ProductRepository repository;
 
-    public List<Product> getAllProduct() { return repository.findAll();}
+    @Cacheable(value = "allProduct")
+    public List<Product> getAllProduct() {
+        return repository.findAll();
+    }
 
-    public Product createProduct(Product product) {return repository.save(product);}
+    @CachePut(value = {"allProduct", "product"}, key = "#id")
+    public Product createProduct(Product product) {
+        return repository.save(product);
+    }
 
-    public Product UpdateProduct(Product product) {return repository.save(product);}
 
-    public void deleteById(Long id) {repository.deleteById(id);}
+    @CachePut(value = {"product", "allProduct"}, key = "#id")
+    public Product UpdateProduct(Product product) {
+        return repository.save(product);
+    }
 
-    public Product findById(Long id) {return repository.findById(id).orElse(null);}
+    @CacheEvict(value = {"product", "allProduct"}, key = "#id")
+    public void deleteById(Long id) {
+        repository.deleteById(id);
+    }
 
+    @Cacheable(value = "product", key = "#id")
+    public Product findById(Long id) {
+        return repository.findById(id).orElse(null);
+    }
+
+    @Cacheable(value = "allProduct", key = "#category")
     public List<Product> findByCategory(String category) {
         return repository.findByCategory(category);
     }
--- storage-service/src/main/java/com/shop/storageservice/Service/StorageService.java
+++ storage-service/src/main/java/com/shop/storageservice/Service/StorageService.java
@@ -3,6 +3,9 @@ package com.shop.storageservice.Service;
 import com.shop.storageservice.Model.Storage;
 import com.shop.storageservice.Repository.StorageRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 import java.util.HashMap;
@@ -14,21 +17,23 @@ public class StorageService {
 
     private final StorageRepository repository;
 
+    @Cacheable(value = "storage", key = "#id")
     public Storage findById(Long id) {
         return repository.findById(id).orElse(null);
     }
 
-
+    @Cacheable(value = "storage", key = "#id")
     public Boolean isInStorage(Long id, Integer requiredquentity) {
-        Storage product = findById(id);
+        Storage product = repository.findById(id).orElse(null);
         return product.getQuantity() >= requiredquentity;
 
     }
-
+    @CachePut(value = "storage", key = "#id")
     public void addProductById(Long addedId, Integer quantityAdded) {
         repository.addProductById(addedId, quantityAdded);
     }
 
+    @CacheEvict(value = "storage", key = "#id")
     public void deleteProductById(Long deletedId, Integer quantityDeleted) {
         repository.deleteproductById(deletedId, quantityDeleted);
     }
@@ -44,10 +49,12 @@ public class StorageService {
         return true;
     }
 
+    @CachePut(value = "storage", key = "#id")
     public void addProductByIdByOne(Long addedId) {
         repository.addProductByIdByOne(addedId);
     }
 
+    @CacheEvict(value = "storage", key = "#id")
     public void deleteProductByIdByOne(Long deletedId) {
         repository.deleteProductByIdByOne(deletedId);
     }
